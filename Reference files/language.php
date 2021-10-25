1e6296df464d220b1201f22049fe92a4ce963016
Surena <78427644+SurenaMHZ@users.noreply.github.com>
2021-10-25 10:02:02 +0330

    Create language.php

---
+++ Reference files/language.php
@@ -0,0 +1,351 @@
+<?php
+	/* 
+	***---------------------***
+	***| Language: English |***
+	***---------------------***
+	*/
+	///Language (Selected language)
+	$lang['Language'] = "en";
+	/// Login
+	$lang['login_title'] = "Login to admin panel";
+	$lang['email'] = "Email";
+	$lang['email_or_username'] = "Email or UserName";
+	$lang['password'] = "Password";
+	$lang['login'] = "Login";
+	$lang['lost_password'] = "Forget your password?";
+	$lang['login_failed'] =  "Email or password incorrect";
+	/// Lost Password
+	$lang['forget_title'] = "Get new password";
+	$lang['get_password'] = "Get password";
+	$lang['forget_error'] =  "<strong>Sorry!</strong>  this email not found.";
+	$lang['forget_success'] =  "We've sent an email to %s.<br>Please click on the password reset link in the email to generate new password. ";
+	$lang['reset_email_subject'] =  "Reset Password";
+	$lang['reset_email_message'] =  "Hello %s!
+We got requested to reset your password, if you do this then just click the following link to reset your password, if not just ignore this email,
+<a href='%s'>Click here to reset your password</a>
+Thank you :)";
+	/// Reset Password
+	$lang['reset_title'] = "Reset password";
+	$lang['hello'] =  "Hello";
+	$lang['new_password'] =  "New Password";
+	$lang['confirm_password'] =  "Confirm Password";
+	$lang['change_password'] =  "Change Password";
+	$lang['reset_info'] =  "You are here to reset your forgetton password.";
+	$lang['reset_error'] =  "No Account Found, Try again";
+	$lang['password_changed'] =  "Password changed";
+	$lang['passwords_not_match'] =  "New Password and confirm password not matchs";
+	/// Header
+	$lang['dashboard'] = "Dashboard";
+	$lang['settings'] = 'Settings';
+	$lang['logout'] = 'Logout';
+	$lang['home'] = 'Home';
+	$lang['user'] = 'users';
+	$lang['select_game'] = 'select game';
+	/// Admin
+	$lang['shortcodes'] = "Shortcodes";
+	$lang['docs'] = "Documentation";
+	$lang['support_site'] = "Support Site";
+	$lang['version_ok'] =  "You are using %s version.";
+	$lang['new_version'] =  "Version %s available now!";
+	/// Settings
+	$lang['Personalization'] =  "Personalization";
+	$lang['Theme'] =  "Theme";
+	$lang['Languages'] =  "Languages";
+	$lang['Viewers'] =  "Viewers";
+	$lang['Savechanges'] =  "Save changes";
+	$lang['Savedchanges'] =  "Saved changes";
+	$lang['change_details'] =  "Change Details";
+	$lang['current_password'] =  "Current Password";
+	$lang['current_password_error'] =  "Current password is wrong";
+	$lang['details_changed'] =  "Details changed";
+	/// Leaderboards
+	$lang['leaderboards'] = "Leaderboards";
+	$lang['leaderboard_name'] = "Leaderboard Name";
+	$lang['leaderboard_token'] =  "Leaderboard Token";
+	$lang['scores'] =  "Scores";
+	$lang['add_score'] =  "Add Score";
+	$lang['delete'] = "Delete";
+	$lang['add'] =  "Add";
+	$lang['yes'] =  "Yes";
+	$lang['cancel'] =  "Cancel";
+	$lang['delete_question'] =  "Are you sure to delete this leaderboard?";
+	$lang['no_leaderboard'] =  "No leaderboard exists. <a href='addleaderboard.php'>Add Leaderboard</a>";
+	$lang['player_name_empty'] =  "Enter player name";
+	$lang['player_score_empty'] =  "Enter player score";
+	$lang['player_id_empty'] =  "Enter player id";
+	/// Add Leaderboard
+	$lang['add_leaderboard'] =  "Add Leaderboard";
+	$lang['leaderboard_reverse'] =  "Scores Sort";
+	$lang['high_to_low'] = 'High to Low';
+	$lang['low_to_high'] = 'Low to High';
+	$lang['database_error'] =  "Error in database";
+	$lang['fill_all_data'] =  "Please fill all details";
+	$lang['leaderboard_created'] =  "Leaderboard created successfully";
+	/// Scores
+	$lang['edit'] =  "Edit";
+	$lang['save'] =  "Save";
+	$lang['player_id'] =  "Player Id";
+	$lang['player_name'] =  "Player Name";
+	$lang['score'] =  "Score";
+	$lang['score_date'] =  "Submit Date";
+	$lang['player_rank'] =  "Player Rank";
+	$lang['no_score'] =  "This leaderboard has no score.";
+	/// Achievements
+	$lang['achievements'] = "Achievements";
+	$lang['achievement_name'] = "Achievement Name";
+	$lang['achievement_token'] =  "Achievement Token";
+	$lang['hits'] =  "Hits";
+	$lang['add_hit'] =  "Add Hit";
+	$lang['delete_achievement_question'] =  "Are you sure to delete this achievement?";
+	$lang['no_achievement'] =  "No achievement exists. <a href='addachievement.php'>Add Achievement</a>";
+	/// Add Achievement
+	$lang['add_achievement'] =  "Add Achievement";
+	$lang['achievement_created'] =  "Achievement created successfully";
+	/// Hits
+	$lang['no_hit'] =  "No player hits this achievement yet.";
+	/// Home
+	$lang['scores_count'] = "Scores Count";
+	$lang['hits_count'] = "Hits Count";
+	$lang['close'] = "Close";
+	$lang['width'] = "Width";
+	$lang['height'] = "Height";
+	$lang['get_code'] = "Get Code";
+	$lang['preview'] = "Preview";
+	$lang['embed_code'] = "Embed Code";
+	$lang['alertnewv'] = "A new version of the system is available";
+	$lang['IVM'] = "Installing a new version of the system is required";
+	$lang['alerObsoleteV'] = "Due to the obsolescence of your system version, the commands are not executed";
+	/// Install
+	$lang['install'] = 'Install';
+	$lang['site_details'] = 'Site Details';
+	$lang['sitename'] = 'Site Name';
+	$lang['database'] = 'Database';
+	$lang['db_host'] = 'Host Name';
+	$lang['db_username'] = 'Database Username';
+	$lang['db_password'] = 'Database Password';
+	$lang['db_name'] = 'Database Name';
+	$lang['admin_user'] = 'Admin Informations';
+	$lang['admin_username'] = 'Admin Username';
+	$lang['admin_email'] = 'Admin Email';
+	$lang['admin_password'] = 'Admin Password';
+	$lang['install_success'] = 'Install Success!';
+	$lang['sitename_desp'] = 'Site name that displayed in all pages title.';
+	$lang['dbhost_desp'] = 'You should be able to get this info from your web host, if localhost doesn’t work.';
+	$lang['dbname_desp'] = 'The name of the database you want to use with WordPress.';
+	$lang['dbuser_desp'] = 'Your database username.';
+	$lang['dbpass_desp'] = 'Your database password.';
+	$lang['adminemail_desp'] = 'Double-check your email address before continuing.';
+	$lang['adminpass_desp'] = 'Important: You will need this password to log in. Please store it in a secure location.';
+	/// Demo
+	$lang['demo_password'] = 'You cant change password in demo mode.';
+	$lang['demo_details'] = 'You cant change details in demo mode.';
+	$lang['demo_version'] = 'You cant edite this version in demo mode.';
+	/// Update
+	$lang['update'] = 'Update';
+	$lang['update_to'] = 'Update to %s version';
+	$lang['update_success'] = 'System successfully updated to version %s.';
+	$lang['update_error'] = 'An error occurred while updating the system. Try again.';
+	/// Versions
+	$lang['versions'] = 'Versions';
+	$lang['add_version'] = 'Add Version';
+	$lang['edit_version'] =  "Edit Version";
+	$lang['update_title'] = 'Update Title';
+	$lang['version_force'] = 'Force Update';
+	$lang['version_link'] = 'Update Link';
+	$lang['version_message'] = 'Message';
+	$lang['version_changes'] = 'Changes';
+	$lang['no_project'] = 'No Project Exists.';
+	$lang['no'] =  "No";
+	$lang['version'] =  "Version";
+	$lang['add_change'] =  "Add New Change";
+	$lang['version_created'] =  "Version created successfully.";
+	$lang['version_edited'] =  "Version edited successfully.";
+	$lang['Changes'] = 'Changes';
+    ///error get post
+	$lang['getVer1'] = "An error occurred while receiving version.";
+	$lang['getVer2'] = "The version with this id does not exist";
+	$lang['getVer3'] = "An error occurred while receiving user hit.";
+	$lang['getVer4'] = "User with this id does not exist.";
+	$lang['getVer5'] = "The achievement with this id does not exist";
+	$lang['getVer6'] = "An error occurred while receiving user rank.";
+	$lang['getVer7'] = "User with this id does not exist.";
+	$lang['getVer8'] = "The leaderboard with this id does not exist";
+	$lang['getVer9'] = "An error occurred while submiting hit";
+	$lang['getVer10'] = "Successfuly sent hit to achievement";
+	$lang['getVer11'] = "Token sent is wrong";
+	$lang['getVer12'] = "The achievement with this id does not exist";
+	$lang['getVer13'] = "An error occurred while sending score";
+	$lang['getVer14'] = "Score sent successfully";
+	$lang['getVer15'] = "Missing Parameter";
+	///games
+	$lang['games'] = 'Games';
+	/// Common
+	$lang['delete'] = "Delete";
+	$lang['add'] =  "Add";
+	$lang['yes'] =  "yes";
+	$lang['no'] =  "No";
+	$lang['cancel'] =  "Cancel";
+	$lang['edit'] =  "Edit";
+	$lang['save'] =  "Save";
+	$lang['close'] = "Close";
+	/// Games
+	$lang['id'] = "Id";
+	$lang['name'] = "Name";
+	$lang['token'] = "Token";
+	$lang['no_game'] =  "No game exists. <a href='#addgame'>Add Game</a>";
+	$lang['manage_values'] =  "Manage Values";
+	$lang['manage_values_title'] =  "Manage %s Values";
+	$lang['manage_variables'] =  "Manage Variables";
+	$lang['manage_variables_title'] =  "Manage %s Variables";
+	$lang['variable_key'] =  "Variable Key";
+	$lang['variable_type'] =  "Variable Type";
+	$lang['variable_default'] =  "Default Value";
+	$lang['add_variable'] =  "Add Variable";
+	$lang['add_variable_success'] =  "The variable has been added successfully.";
+	$lang['edit_variable_success'] =  "The variable has been successfully edited.";
+	$lang['delete_variable_success'] =  "The variable has been successfully deleted.";
+	$lang['delete_variable_question'] =  "Are you sure you want to remove this variable?";
+	$lang['duplicate_variable_error'] =  "There is a variable with this name.";
+	$lang['delete_game_question'] =  "Are you sure you want to remove this game?";
+	$lang['game_deleted_success'] =  "The game was successfully removed.";
+	$lang['game_deleted_failed'] =  "There was an error deleting the game.";
+	$lang['game_edit_success'] =  "The game was successfully edited.";
+	$lang['game_edit_failed'] =  "There was an error editing the game.";
+	$lang['no_variable'] =  "This game is still not has variable. Create a new variable from the form below.";
+	$lang['no_value'] =  "There is no value.";
+	$lang['add_game'] =  "Add Game";
+	//$lang['database_error'] =  "Database error";
+	$lang['fill_all_data'] =  "Please fill all data";
+	$lang['game_created'] =  "Game Added.";
+	$lang['add_value'] =  "Add Value";
+	$lang['user_id'] =  "User Id";
+	$lang['add_value_success'] =  "Value added successfully.";
+	$lang['delete_value_success'] =  "Value deleted successfully.";
+	$lang['delete_value_failed'] =  "There was an error deleting this value.";
+	$lang['delete_value_question'] =  "Are you sure you want to delete this value?";
+	$lang['duplicate_value_error'] =  "There is some value with this user id.";
+	$lang['click_for_edit'] =  "Click to edit";	
+	///users
+	$lang['manage_user'] =  "Manage User";
+	$lang['add_user_success'] =  "user added successfully.";
+	$lang['duplicate_user_error'] =  "There is some user with this user id.";
+	$lang['manage_users_title'] =  "Manage %s users";
+	$lang['add_user'] =  "Add user";
+	$lang['no_user'] =  "There is no user.";
+	$lang['delete_user_question'] =  "Are you sure you want to delete this user?";
+	$lang['delete_user_success'] =  "user deleted successfully.";
+	$lang['delete_user_failed'] =  "There was an error deleting this user.";
+	///hit
+	$lang['delete_Achievement_success'] =  "Achievement deleted successfully.";
+	$lang['delete_Achievement_failed'] =  "There was an error deleting this Achievement.";
+	$lang['add_hit_success'] =  "hit added successfully.";
+	///Version
+	$lang['delete_Version_success'] =  "Version deleted successfully.";
+	$lang['delete_Version_failed'] =  "There was an error deleting this Version.";
+	$lang['delete_Leaderboard_success'] =  "Leaderboard deleted successfully.";
+	$lang['delete_Leaderboard_failed'] =  "There was an error deleting this Leaderboard.";
+	///score
+	$lang['add_score_success'] =  "score added successfully.";
+	$lang['add_score_failed'] =  "score added failed.";
+	///admins
+	$lang['administrator'] =  "TheManager";
+	$lang['administrator_created'] =  "the manager created successfully.";
+	$lang['add_administrator'] =  "Add the manager";
+	$lang['administrator_name'] =  "username the manager";
+	$lang['administrator_email'] =  "email the manager";
+	$lang['administrator_password'] =  "password the manager";
+	$lang['no_administrator'] =  "No the manager exists.";
+	$lang['administrator_name_empty'] =  "Enter the manager Name";
+	$lang['administrator_email_empty'] =  "Enter the manager Email";
+	$lang['administrator_pass_empty'] =  "Enter Password the manager";
+	$lang['delete_administrator_success'] =  "the manager deleted successfully.";
+	$lang['delete_administrator_failed'] =  "There was an error deleting this the manager.";
+	$lang['edite_administrator_success'] =  "the manager edite successfully.";
+	$lang['edite_administrator_failed'] =  "There was an error editing this the manager.";
+	$lang['QDadministrator'] = "Are you sure you want to remove this manager?";
+	$lang['delete_administrator_success'] =  "manager deleted successfully.";
+	$lang['delete_administrator_failed'] =  "There was an error deleting this manager.";
+	///users
+	$lang['delete_users_success'] =  "users Game deleted successfully.";
+	$lang['delete_users_failed'] =  "There was an error deleting this users Game.";
+	///mail
+	$lang['Contact_the_manufacturer'] =  "Contact the manufacturer";
+	$lang['Post_comment'] =  "Post a comment";
+	$lang['Messagecomment']= "Message";
+	$lang['Message_empty'] =  "Enter Message";
+	$lang['Post_comment_success'] =  "Post comment successfully.";
+	$lang['Post_comment_failed'] =  "Post comment failed.";
+	$lang['send'] =  "send";
+	///Modules
+	$lang['InstalleModule'] = "Installe Module";
+	$lang['InstallerModule'] = "Installer Module";
+	$lang['Modules'] = "Modules";
+	$lang['successIM'] = "The module was successfully installed";
+	$lang['ErrorM'] = "Error installing module";
+	$lang['ErrorUM'] = "Error in uploading Module";
+	$lang['Moduleelection1'] = "Please select a file that has the extension *((ssm) OR (ssb))* (Module type) ";
+	$lang['Moduleelection2'] = "Please select a file";
+	$lang['Version'] = "Version";
+	$lang['InstalledModule'] = "Installed Module";
+	$lang['no_module'] = "No module installed";
+	$lang['Laccess'] = "Last access";
+	$lang['Lichange'] = "Last inode change";
+	$lang['Information'] = "Information";
+	$lang['View'] = "View";
+	$lang['github'] = "Github";
+	$lang['Manufacturername'] = "author";
+	$lang['Manufacturer_Web'] = "Manufacturer Web";
+	$lang['Manufacturer_Gmail'] = "Manufacturer Gmail";
+	$lang['actions'] = "Actions";
+	$lang['params'] = "Params";
+	$lang['Description'] = "Description";
+	$lang['type'] = "Type";
+	$lang['notfoundinfo'] = "Information.json file not found";
+	$lang['notfoundac'] = "action.php file not found";
+	$lang['notfoundin'] = "index.php file not found";
+	$lang['notfoundlan'] = "language.php file not found";
+	$lang['notfoundtheme'] = "Theme.css file not found";
+	$lang['notmodule'] = "This module does not follow the rules and the system is not able to install this module";
+	$lang['Document'] = "Document";
+	///backup
+	$lang['backup'] = "Backup";
+	$lang['backups'] = "Backup successful";
+	$lang['upbackups'] = "uploading Backup successful";
+	$lang['backupe'] = "Backup error";
+	/*
+	$lang['Year'] = "Year";
+	$lang['Month'] = "Month";
+	$lang['Day'] = "Day";
+	$lang['Hour'] = "Hour";
+	$lang['minute'] = "minute";
+	$lang['Second'] = "Second";
+		$lang['noteib'] = "(Note: This will delete all your current data and replace it with the information in this backup)";
+	$lang['ssks'] = "If you are using another system backup, be sure to introduce the same system key because the operation will encounter an error";
+	*/
+	$lang['Installe'] = "Installe";
+	$lang['QIbackup'] = "Do you want to install this backup?  Note: This will delete all your current data and replace it with the information in this backup  Warning: If you are using another system backup, be sure to introduce the same system key because the operation will encounter an error";
+
+	$lang['sib'] = "The backup was successfully installed";
+	$lang['Erroris'] = "Error installing backup";
+	$lang['download'] = "Download";
+	$lang['Nobackup'] = "No backup";
+	$lang['QDbackup'] = "Are you sure you want to remove this backup?";
+	$lang['delete_backup_success'] =  "backup deleted successfully.";
+	$lang['delete_backup_failed'] =  "There was an error deleting this backup.";
+	$lang['Backups'] = "Backups";
+	$lang['nameanddate'] = "( Hour - minute - Second _ Day - Month - Year )";
+	//ssk
+	$lang['ssklink'] = "Surena.ssk file link";
+	$lang['ssk'] = "Surena System Key";
+	$lang['slink'] = "save link";
+	$lang['slinks'] = "The link was saved";
+	//
+	$lang['euregistered'] = "This email and username are already registered";
+	$lang['eregistered'] = "This email has already been registered";
+	$lang['uregistered'] = "This username is already registered";
+	$lang['asc'] = "Account successfully created";
+	$lang['validlogin'] = "The information entered is not valid for login";
+	$lang['Font'] = "Font";
+	$lang['TIEI'] = "The information entered is incorrect";
+	$lang['addonsc2c3'] = "Addon construct2 and construct3";
+?>
